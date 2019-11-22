417d5205417b672e6860c7ef95072c81ae795ab6
Ryan Perretta <ryan.perretta@gmail.com>
2019-11-21 19:47:22 -0500

    Add files via upload

--- anagramfinder.cpp
+++ anagramfinder.cpp
@@ -12,7 +12,7 @@
 using namespace std;
 
 vector<string> dict;
-int letter_vals[] = { 2, 3, 5, 7, 11 , 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 71, 73, 79, 83, 89, 97, 101 };
+int letter_vals[] = { 2, 3, 5, 7, 11 , 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 71, 73, 79, 83, 89, 97, 101, 103 };
 
 //Calculates the unique product for a string
 int get_product(string s) {
